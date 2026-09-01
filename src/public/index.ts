da515febf61e0e04cc56555db2390a89e3d0da85
Gaius Peteros <grpeteros06@gmail.com>
2026-09-01 12:21:39 +0800

    Fixed error on deploy

---
+++ src/public/index.ts
@@ -0,0 +1,19 @@
+import express, { type Express, type Request, type Response } from 'express';
+import cors from 'cors';
+const app: Express = express();
+
+app.use(cors());
+app.get('/projects', (req: Request, res: Response) => {
+  res.send({
+    projects: [
+      { name: 'GovStack', description: 'An Umbraco CMS product. Utilizing C#.NET and VueJS.' },
+      { name: 'Harley Davidson Cebu(RDAK)', description: 'An e-service software for Harley Davidson dealers. Utilizing ReactJS, React Native and Java Springboot.' },
+      { name: 'Alliance WebPOS', description: 'A web-based point-of-sale system. Utilizing ReactJS, React Native and PHP Laravel.' }
+    ]
+  });
+});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World Test!');
+});
+
+app.listen(3000);
