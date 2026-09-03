88e94c95d6c2fdd7617235dd531cf0b5acc5277d
Gaius Peteros <grpeteros06@gmail.com>
2026-09-03 10:21:40 +0800

    Added supabase connection

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,25 @@
 import express, { type Express, type Request, type Response } from 'express';
 import cors from 'cors';
+import { createClient } from '@supabase/supabase-js';
+
+
+console.log('Supabase URL:', process.env.VITE_SUPABASE_URL);
+const supabase = createClient(
+  process.env.VITE_SUPABASE_URL as string || 
+  '',
+  process.env.VITE_SUPABASE_SECRET_KEY as string || ''
+);
+
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.get('/projects', (req: Request, res: Response) => {
+app.get('/projects', async (req: Request, res: Response) => {
+  let { data: projectsData, error } = await supabase
+  .from('projects')
+  .select('*')
   res.send({
-    projects: [
-      { name: 'GovStack', description: 'An Umbraco CMS product. Utilizing C#.NET and VueJS.' },
-      { name: 'Harley Davidson Cebu(RDAK)', description: 'An e-service software for Harley Davidson dealers. Utilizing ReactJS, React Native and Java Springboot.' },
-      { name: 'Alliance WebPOS', description: 'A web-based point-of-sale system. Utilizing ReactJS, React Native and PHP Laravel.' }
-    ]
+      projectsData,
   });
 });
 app.get('/', (req: Request, res: Response) => {
