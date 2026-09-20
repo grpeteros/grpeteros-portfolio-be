b889d81bc23255e448ee51d346a87c0f7770f5c7
Gaius Peteros <grpeteros06@gmail.com>
2026-09-20 14:12:59 +0800

    Added print to excel feature

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Express, type Request, type Response } from 'express';
 import cors from 'cors';
 import { createClient } from '@supabase/supabase-js';
+import ExcelJS from 'exceljs';
 
 
 console.log('Supabase URL:', process.env.VITE_SUPABASE_URL);
@@ -49,11 +50,47 @@ app.get('/sales', async (req: Request, res: Response) => {
   let { data: sales, error } = await supabase
     .from('transactions')
     .select('*').gte('created_at', new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toISOString())
-    .lte('created_at', new Date().toISOString());  
+    .lte('created_at', new Date().toISOString());
   res.send({
     transactions: sales
   });
 
+});
+
+app.get('/sales/print', async (req: Request, res: Response) => {
+  let { data: sales, error } = await supabase
+    .from('transactions')
+    .select('*').gte('created_at', new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toISOString())
+    .lte('created_at', new Date().toISOString());
+
+  let sortedData = sales?.sort((a: any, b: any) => b.id - a.id)
+
+  let total_price = 0.0
+  let workbook = new ExcelJS.Workbook()
+  let worksheet = workbook.addWorksheet('transactions')
+   worksheet.columns = [
+    { header: 'Date', key: 'created_at', width: 20 },
+    { header: 'Products', key: 'products', width: 30 },
+    { header: 'Total Price', key: 'total_price', width: 20 },
+  ];
+
+  sortedData?.map((transaction) => {
+    transaction.products = transaction.products.map((product: any) => {
+      return `${product.product_name} (x${product.product_quantity})`;
+    });
+    total_price += transaction.total_price;
+    worksheet.addRow({ created_at: new Date(transaction.created_at).toLocaleDateString(), products: JSON.stringify(transaction.products), total_price: transaction.total_price });
+  });
+
+
+  worksheet.addRow({ products: 'Total:', total_price: total_price });
+
+  res.setHeader('Content-Disposition', 'attachment; filename='  + encodeURIComponent('transactions.xlsx'));
+  res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  
+  await workbook.xlsx.write(res).then(() => {
+    res.status(200).end();
+  });
 });
 
 app.post('/cart/finish', async (req: Request, res: Response) => {
